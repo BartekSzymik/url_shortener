717c7f2ffdb3a36d9b512ff82f56c7a64657cb9d
Bartosz Szymik <szymik.bartek@gmail.com>
2023-08-06 01:54:32 +0200

    postgres setup according to 12factorapp rules

--- api/config/settings.py
+++ api/config/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,6 +79,10 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+DB_CONNECTION_STRING = os.environ.get("DB_CONNECTION_STRING")
+
+DATABASES = {"default": dj_database_url.config(default=DB_CONNECTION_STRING)}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
