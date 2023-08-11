5bf8362ebb1d153366cdcb42f0160c7f07e601a3
Bartosz Szymik <szymik.bartek@gmail.com>
2023-08-11 07:40:39 +0200

    swagger schema configuration

--- api/shortener/views.py
+++ api/shortener/views.py
@@ -9,6 +9,24 @@ from .serializers import URLSerializer
 from typing import Any
 
 class CreateShortURLApiView(generics.CreateAPIView):
+    """
+    create:
+    Creates a short URL from the provided original URL.
+
+    Expects JSON data in the format:
+    {
+        "url": "http://example.com"
+    }
+
+    Where "url" is the original URL that is to be shortened.
+
+    The response contains the original URL and the shortened URL:
+    {
+        "url": "http://example.com",
+        "short_url": "https://obscure-reef-55802-1aae042f012c.herokuapp.com/abcde/"
+    }
+    """
+
     queryset = models.URL.objects.all()
     serializer_class = serializers.OriginalURLSerializer
 
@@ -21,6 +39,19 @@ class CreateShortURLApiView(generics.CreateAPIView):
 
         return Response(data=short_url.data, status=status.HTTP_201_CREATED)
 
+
+class ShortUrlRedirectView(RedirectView):
+    """
+    get:
+    Redirects the user to the original URL from the provided short URL.
+
+    Expects the short URL to be part of the path, like:
+    https://obscure-reef-55802-1aae042f012c.herokuapp.com/abcde/
+
+    Where "abcd" is the short URL.
+
+    The response is a HTTP 302 redirect to the original URL.
+    """
 
 def get_redirect_url(self, *args: Any, short_url: str,
                      **kwargs: Any) -> str | None:
